27495eca68b8e4016b778ca59ede18bf59316be6
MauryaAayush <maayush235@gmail.com>
2024-02-21 14:34:53 +0530

    Radhey Radhey

--- lib/view/cartscreen.dart
+++ lib/view/cartscreen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../utils/Cartlist.dart';
 
-void main()
-{
+void main() {
   runApp(const CartScreen());
 }
 
@@ -16,19 +15,17 @@ class CartScreen extends StatefulWidget {
 
 class _CartScreenState extends State<CartScreen> {
   int sum() {
-   int total = 0;
+    int total = 0;
 
-   for(var item in cart)
-     {
-      int price =item['price'];
+    for (var item in cart) {
+      int price = item['price'];
       total += price * count;
-      }
-   return total;
- }
+    }
+    return total;
+  }
 
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
@@ -115,117 +112,132 @@ class _CartScreenState extends State<CartScreen> {
               width: width,
               // color: Colors.yellow,
               child: Row(
-
                 children: [
                   Column(
                     children: [
                       SizedBox(
                         height: height,
-                        width: width*0.7,
-                        child:Container(
+                        width: width * 0.7,
+                        child: Container(
                           margin: const EdgeInsets.fromLTRB(50, 50, 50, 0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Text('Your Shopping cart',style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600
-                              ), ),
-
-                              Column(
-                                children: List.generate(cart.length, (index) => Column(
-                                  children: [
-                                    Container(
-                                      padding: const EdgeInsets.all(20),
-                                      margin: const EdgeInsets.symmetric(vertical: 20),
-                                      height: height*0.15,
-                                      width: width,
-                                      decoration: const BoxDecoration(
-                                          color: Color(0xFFF5F5F5)
-                                      ),
-                                      child: Row(
-
-                                        children: <Widget>[
-                                          Container(
-                                            height: height*0.1,
-                                            width: width*0.05,
-                                            decoration: const BoxDecoration(
-                                                color: Colors.white
-                                            ),
-                                            child: Image.asset(cart[index]['img'],fit: BoxFit.cover,),
-                                          ),
-
-                                          const SizedBox(width: 50,),
-
-                                          Column(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text(cart[index]['name']),
-                                              Text('${cart[index]['price']}'),
-                                            ],
-                                          ),
-
-                                  const SizedBox(width: 150,),
-
-                                  Row(
-                                    children: [
-
-                                      InkWell(
-                                          onTap: () {
-                                            if(count > 1)
-                                              {
-                                                setState(() {
-                                                  count--;
-                                                });
-                                              }
-                                          },
-                                          child: const Icon(Icons.remove)),
-
-                                      Container(
-                                        alignment: Alignment.center,
-                                        height: 40,
-                                        width: 40,
-                                        margin: const EdgeInsets.all(5),
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadiusDirectional.circular(8),
-                                            border: Border.all(color: Colors.grey),
-                                            color: Colors.white),
-                                        child: Text('$count'),
-                                      ),
-                                              InkWell(
-                                                onTap: () {
-                                                  setState(() {
-                                                    count++;
-                                                  });
-                                                },
-
-                                                  child: const Icon(Icons.add)),
-                                    ],
-                                  ),
-
-                                          const SizedBox(width: 350,),
-
-                                          InkWell(
-                                            onTap: () {
-                                              setState(() {
-                                                cart.removeAt(index);
-                                              });
-                                            },
-                                            child: Icon(
-                                              Icons.delete,
-                                              color: Colors.black,
-                                              size: 30,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                )),
+                              const Text(
+                                'Your Shopping cart',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600),
                               ),
-
+                              Column(
+                                children: List.generate(
+                                    cart.length,
+                                    (index) => Column(
+                                          children: [
+                                            Container(
+                                              padding: const EdgeInsets.all(20),
+                                              margin:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 20),
+                                              height: height * 0.15,
+                                              width: width,
+                                              decoration: const BoxDecoration(
+                                                  color: Color(0xFFF5F5F5)),
+                                              child: Row(
+                                                children: <Widget>[
+                                                  Container(
+                                                    height: height * 0.1,
+                                                    width: width * 0.05,
+                                                    decoration:
+                                                        const BoxDecoration(
+                                                            color:
+                                                                Colors.white),
+                                                    child: Image.asset(
+                                                      cart[index]['img'],
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 50,
+                                                  ),
+                                                  Column(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment.start,
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(cart[index]['name']),
+                                                      Text(
+                                                          '${cart[index]['price']}'),
+                                                    ],
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 150,
+                                                  ),
+                                                  Row(
+                                                    children: [
+                                                      InkWell(
+                                                          onTap: () {
+                                                            if (count > 1) {
+                                                              setState(() {
+                                                                count--;
+                                                              });
+                                                            }
+                                                          },
+                                                          child: const Icon(
+                                                              Icons.remove)),
+                                                      Container(
+                                                        alignment:
+                                                            Alignment.center,
+                                                        height: 40,
+                                                        width: 40,
+                                                        margin: const EdgeInsets
+                                                            .all(5),
+                                                        decoration: BoxDecoration(
+                                                            borderRadius:
+                                                                BorderRadiusDirectional
+                                                                    .circular(
+                                                                        8),
+                                                            border: Border.all(
+                                                                color: Colors
+                                                                    .grey),
+                                                            color:
+                                                                Colors.white),
+                                                        child: Text('$count'),
+                                                      ),
+                                                      InkWell(
+                                                          onTap: () {
+                                                            setState(() {
+                                                              count++;
+                                                            });
+                                                          },
+                                                          child: const Icon(
+                                                              Icons.add)),
+                                                    ],
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 350,
+                                                  ),
+                                                  InkWell(
+                                                    onTap: () {
+                                                      setState(() {
+                                                        cart.removeAt(index);
+                                                      });
+                                                    },
+                                                    child: Icon(
+                                                      Icons.delete,
+                                                      color: Colors.black,
+                                                      size: 30,
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        )),
+                              ),
                             ],
                           ),
                         ),
@@ -246,73 +258,90 @@ class _CartScreenState extends State<CartScreen> {
                               fontSize: 16,
                             ),
                           ),
-
-                          SizedBox(height: 50,),
-
-
+                          SizedBox(
+                            height: 50,
+                          ),
                           Row(
                             children: [
-                              Text('Sub total',style: TextStyle(
-                                color: Colors.black,
-                              ),),
-                              SizedBox(width: 200,),
-                              Text('${sum()} RWF',style: TextStyle(
-                                color: Colors.black,
-                              ),),
+                              Text(
+                                'Sub total',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 200,
+                              ),
+                              Text(
+                                '${sum()} RWF',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
                             ],
                           ),
-
-                          SizedBox(height: 20,),
-
+                          SizedBox(
+                            height: 20,
+                          ),
                           Row(
                             children: [
-                              Text('Delivery fee',style: TextStyle(
-                                color: Colors.black,
-                              ),),
-                              SizedBox(width: 200,),
-                              Text('0RWF',style: TextStyle(
-                                color: Colors.black,
-                              ),),
+                              Text(
+                                'Delivery fee',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 200,
+                              ),
+                              Text(
+                                '0RWF',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
                             ],
                           ),
-                          SizedBox(height: 20,),
-
+                          SizedBox(
+                            height: 20,
+                          ),
                           Container(
                             height: 2,
                             width: 380,
                             color: Colors.black,
                           ),
-
-                          SizedBox(height: 20,),
-
+                          SizedBox(
+                            height: 20,
+                          ),
                           Row(
                             children: [
-                              SizedBox(width: 262,),
-                              Text('${sum()} RWF',style: TextStyle(
-                                color: Colors.black,
-                              ),),
+                              SizedBox(
+                                width: 262,
+                              ),
+                              Text(
+                                '${sum()} RWF',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
                             ],
                           ),
-
                           Divider(
-
                             color: Colors.blueAccent,
                             thickness: 10,
                           ),
-
                           Container(
                             alignment: Alignment.center,
-                            height: height*0.06,
-                            width: width*0.1,
+                            height: height * 0.06,
+                            width: width * 0.1,
                             decoration: BoxDecoration(
                               color: Colors.red,
-
                             ),
-                            child: Text('Proceed to checkout',style: TextStyle(
-                                color: Colors.white
-                            ),),
+                            child: Text(
+                              'Proceed to checkout',
+                              style: TextStyle(color: Colors.white),
+                            ),
                           )
-
                         ],
                       ),
                     ),
@@ -324,7 +353,7 @@ class _CartScreenState extends State<CartScreen> {
         ),
       ),
     );
-
   }
 }
+
 int count = 1;
